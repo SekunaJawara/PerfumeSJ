9104a649641c687808b952de947e3456126d8795
Seku <sjawara@institutmvm.cat>
2026-05-26 03:17:17 +0200

    Fix Vite manifest: add carousel.js and comparator.js as entry points, remove missing carousel.css

--- resources/views/partials/_carousel.blade.php
+++ resources/views/partials/_carousel.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/js/carousel.js', 'resources/css/carousel.css'])
+    @vite(['resources/js/carousel.js'])
 </head>
 <body>
     <div id="carousel"></div>
